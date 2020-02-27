c382113cf204bc5a7b5cdb7e6c37403a98fd1e68
steven126 <stevenegan10@gmail.com>
2020-02-27 09:25:26 -0700

    added controllers

--- main/server/index.js
+++ main/server/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const sessionManagement = require("./controllers/sessionManagement");
+const userManagement = require("./controllers/userManagement");
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/login", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
