b4c9f0c99f85c8472e415c6cd76f8ef6524a9516
Eli <40049026+Bush2775@users.noreply.github.com>
2020-02-29 10:04:57 -0700

    userManagement

--- main/server/index.js
+++ main/server/index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 
 app.post("/login", (req, res) => {
   console.log("hit");
+  userManagement.login(req.body);
   console.log(req.body);
   res.send("Hello World!");
 });
