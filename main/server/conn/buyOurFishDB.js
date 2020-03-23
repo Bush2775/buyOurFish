6eae9cd04cb2d868c28d62c3eeb94ce25914125b
Eli Bush <eli.h.bush@gmail.com>
2020-03-23 15:32:50 -0600

    changing user

--- main/server/conn/buyOurFishDB.js
+++ main/server/conn/buyOurFishDB.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("fishData", "chuck", "intersect", {
+const db = new Sequelize("fishData", "postgres", "buyourfish", {
   host: "192.168.50.65",
   port: 5432,
   dialect: "postgres"
